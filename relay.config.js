52e7c8a13664adf77ff7d479227815b56a6bd3a2
JSer <69352453+JSerZANP@users.noreply.github.com>
2022-10-01 10:28:30 +0100

    bootstrap

---
+++ relay.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  src: "./src",
+  language: "typescript", // "javascript" | "typescript" | "flow"
+  schema: "./data/schema.graphql",
+  exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
+};
